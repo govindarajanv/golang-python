a81f772c21328344cb93f985b0725cb4736b4f08
Govindarajan V <govindarajanv@gmail.com>
2023-08-10 18:13:29 +0530

    Update singleton_decorator.py

--- python/programs/singleton_decorator.py
+++ python/programs/singleton_decorator.py
@@ -1,11 +1,11 @@
-def singleton(func):
+def singleton(cls_):
     _instances = {}
 
     # *args and **kwargs to denote thw function's arguments
     def get_instance(*args, **kwargs):
-        if func not in _instances:
-            _instances[func] = func(*args, **kwargs)
-        return _instances[func]
+        if cls_ not in _instances:
+            _instances[cls_] = cls_(*args, **kwargs)
+        return _instances[cls_]
 
     return get_instance
 
