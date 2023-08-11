a3388ac3861a1b4728c3d16068c0ea26a9da3b0b
Govindarajan V <govindarajanv@gmail.com>
2023-08-11 11:18:52 +0000

    factory refactoring

--- python/programs/factory.py
+++ python/programs/factory.py
@@ -1,15 +1,20 @@
 class Tea:
-    
+
+    def __init__(self,flavour="Plain"):
+        self.flavour = flavour
+
     def consume(self):
-        print ("Tea is getting consumed")
+        print (f"{self.flavour} Tea is getting consumed")
 
 class TeaFactory:
 
     @staticmethod
-    def prepare():
-        print ("Preparing Tea")
-        return Tea()
+    def prepare(flavour="Plain"):
+        print (f"{flavour} Tea is getting prepared")
+        return Tea(flavour)
 
 if __name__ == "__main__":
     tea = TeaFactory.prepare()
+    print (tea.consume())
+    tea = TeaFactory.prepare("Ginger")
     print (tea.consume())
