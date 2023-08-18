5f71a981b5bb28cc2fdee0322bf95697ba09cb8e
Govindarajan V <govindarajanv@gmail.com>
2023-08-18 12:59:41 +0000

    adapter pattern

--- python/programs/structural-design-patterns/adaptor.py
+++ python/programs/structural-design-patterns/adaptor.py
@@ -3,28 +3,28 @@ class TypeA_Plug:
     def __init__(self):
         self.current = "15 A"
         self.voltage = "110 V"
-    
-    def connect(self,socket):
-        ret = True
-        if (self.current != socket.current or self.voltage != socket.voltage):
-            ret = False
-        return ret 
 
     def deliver(self):
-        return self.current, self.voltage
+        return f" Charging with {self.current}/{self.voltage}"
 
     def __str__(self):
         return f"Plug Type A ({self.current}/{self.voltage})\n"
 
 class TypeA_Socket:
 
-    def __init__(self):
+    def __init__(self,plug):
         self.current = "15 A"
         self.voltage = "110 V"
+        self.plug = None
+        self.connected =False
     
-    def deliver(self,plug):
-        self.current = current
-        self.voltage = voltage
+    def switch_on(self):
+        if (self.current == plug.current and self.voltage == plug.voltage):
+            self.plug.deliver()
+            return True
+        else:
+            print ("Not Charging")
+            return False
 
     def __str__(self):
         return f"Socket Type A ({self.current}/{self.voltage})\n"
@@ -61,24 +61,28 @@ class Mobile:
     def __init__(self, battery_percent):
         self.battery_percent = battery_percent
     
-    def charge_me(self,plug,socket):
-        print ("We are charging a mobile having battery {}%  with {} and {}\n".format(self.battery_percent,plug,socket))
-        if plug.connect(socket):
-            self.battery_percent = str(int(self.battery_percent) + 1)
-            return f"Charging!!! Battery {self.battery_percent}%\n"
-        else:
-            return f"Not Charging!!! Battery {self.battery_percent}%\n"
+    def charge_me(self,socket):
+        print ("We are charging a mobile having battery {}%  with {} and {}\n".format(self.battery_percent,socket.plug,socket))
+
+        status = socket.switch_on()
+        #self.battery_percent = str(int(self.battery_percent) + 1)
+        return f"{status}!!! Battery {self.battery_percent}%\n"
+
 
 
 if __name__ == "__main__":
     
     mobile = Mobile("10")
     plug = TypeA_Plug()
-    socket = TypeA_Socket()
-    print (mobile.charge_me(plug,socket))
+    socket = TypeA_Socket(plug)
+    print (mobile.charge_me(socket))
 
-    mobile = Mobile("20")
-    plug = TypeA_Plug()
-    socket = TypeC_Socket()
-    print (mobile.charge_me(plug,socket))
-    
+ #   mobile = Mobile("20")
+ #   plug = TypeA_Plug()
+ #   socket = TypeC_Socket()
+ #   print (mobile.charge_me(plug,socket))
+
+ #   mobile = Mobile("20")
+ #   plug = TypeA_Plug()
+ #   socket = TypeC_Socket()
+ #   print (mobile.charge_me(plug,socket))   
