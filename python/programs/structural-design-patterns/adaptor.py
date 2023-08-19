257c491afa4d8bfd1373cf55a417d91753f263fc
Govindarajan V <govindarajanv@gmail.com>
2023-08-19 04:26:59 +0000

    adapter.py

--- python/programs/structural-design-patterns/adaptor.py
+++ python/programs/structural-design-patterns/adaptor.py
@@ -3,10 +3,35 @@ class MobileAdapter:
         self.out_voltage = 0
         self.out_ampere = 0
 
-    def connect(self)
+    def deliver(self,in_voltage,in_ampere):
+        if in_voltage == 220:
+            self.out_voltage = 5
+        else:
+            self.out_voltage = 0
+        if in_ampere == 5:
+            self.out_ampere = 2.1
+        else:
+            self.out_ampere = 0
+
 
 class  Socket:
-    pass
+    def __init__(self):
+        self.adapter = None
+        self.switched_on = False
+
+    def connect(self,adapter):
+        self.adapter = adapter
+
+    def switch_on(self):
+        self.switched_on = True
+        if self.adapter:
+            self.adapter.deliver()
+    def switch_off(self):
+        self.switched_on = False
+        if self.adapter:
+            self.adapter.in_voltage = 0
+            self.adapter.in_ampere = 0
+
 
 class Mobile:
     def __init__(self):
@@ -25,7 +50,11 @@ class Mobile:
 
 
 if __name__ == "__main__":
+
+    socket = Socket()
+
     mobile = Mobile()
     adapter = MobileAdapter()
+    socket.connect(adapter)
     mobile.connect(adapter)
     print (mobile.charge_status())
