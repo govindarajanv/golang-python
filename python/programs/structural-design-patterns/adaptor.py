677d13390fd6f62e86d47c20f34da97d80081d75
Govindarajan V <govindarajanv@gmail.com>
2023-08-19 05:16:15 +0000

    adapter design pattern

--- python/programs/structural-design-patterns/adaptor.py
+++ python/programs/structural-design-patterns/adaptor.py
@@ -3,7 +3,7 @@ class MobileAdapter:
         self.out_voltage = 0
         self.out_ampere = 0
 
-    def deliver(self,in_voltage,in_ampere):
+    def transform(self,in_voltage,in_ampere):
         if in_voltage == 220:
             self.out_voltage = 5
         else:
@@ -17,44 +17,57 @@ class MobileAdapter:
 class  Socket:
     def __init__(self):
         self.adapter = None
-        self.switched_on = False
 
     def connect(self,adapter):
         self.adapter = adapter
 
-    def switch_on(self):
-        self.switched_on = True
+    def power_on(self):
         if self.adapter:
-            self.adapter.deliver()
-    def switch_off(self):
-        self.switched_on = False
+            self.adapter.transform(220,5)
+    def power_off(self):
         if self.adapter:
-            self.adapter.in_voltage = 0
-            self.adapter.in_ampere = 0
-
+            self.adapter.transform(0,0)
 
 class Mobile:
     def __init__(self):
-        self.__charging = False
+        self.adapter = None
         self.__voltage = 5
         self.__ampere = 2.1
+        self.__charging = False
 
     def connect(self,adapter):
-        if adapter.out_voltage == self.__voltage and adapter.out_ampere == self.__ampere:
+        self.adapter = adapter
+
+    def charge_status(self):
+        if self.adapter.out_voltage == self.__voltage and self.adapter.out_ampere == self.__ampere:
             self.__charging = True
         else:
             self.__charging = False
-
-    def charge_status(self):
-        return self.__charging
-
+        return f"\n-> Mobile charging: {self.__charging}\n"
 
 if __name__ == "__main__":
 
     socket = Socket()
 
     mobile = Mobile()
+
     adapter = MobileAdapter()
-    socket.connect(adapter)
+
     mobile.connect(adapter)
+    print ("mobile connected to adapter")
+
+    socket.connect(adapter)
+    print ("socket connected to adapter")
+
     print (mobile.charge_status())
+
+    socket.power_on()
+    print ("socket is powered on")
+
+    print (mobile.charge_status())
+
+    socket.power_off()
+    print ("socket is powered off")
+
+    print (mobile.charge_status())
+
