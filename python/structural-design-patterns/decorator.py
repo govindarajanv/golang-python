af2be5fd9c66a6106cf1d205b8b0cf4ddef4b9e1
Govindarajan V <govindarajanv@gmail.com>
2023-09-07 11:28:05 +0000

    flyyweight

--- python/structural-design-patterns/decorator.py
+++ python/structural-design-patterns/decorator.py
@@ -1,42 +1,43 @@
 class FileWrapper:
 
-  def __init__(self, file1):
-    self.file2 = file1
-    print (self.__dict__)
+  def __init__(self, file):
+    self.fileObj = file
 
   def __iter__(self):
     print ("__next__ is called")
-    return self.file2.__iter__()
+    return self.fileObj.__iter__()
 
   def __next__(self):
     print ("__next__ is called")
-    return self.file2.__next__()
+    return self.fileObj.__next__()
 
   def __getattr__(self, item):
-    return getattr(self.__dict__['file2'], item)
+
+    return getattr(self.__dict__['fileObj'], item)
+
 
   def __setattr__(self, key, value):
-    if key == 'file2':
+    if key == 'fileObj':
       self.__dict__[key] = value
     else:
-      setattr(self.__dict__['file2'], key,value)
+      setattr(self.__dict__['fileObj'], key,value)
+
 
   def __delattr__(self, item):
-    delattr(self.__dict__['file2'], item)
+    delattr(self.__dict__['fileObj'], item)
 
 
 if __name__ == '__main__':
-  test = open('decorator.txt', 'w')
   file = FileWrapper(open('decorator.txt', 'w'))
-  #print (file1.__dict__)
-
   file.write('testing')
   file.writelines("""
 first line
 second line
+third line
   """)
   file.close()
-  print (file.__dict__["file2"].__dict__)
-  setattr(file,"page", 32)
-  print (file.__dict__["file2"].__dict__)
+  print ("Now let us render the contents",end="\n")
+  with open("decorator.txt") as new_file:
+    print(new_file.read())
+
 
