2f09f7a9b647221690ec8cf2b7f90b081c26c026
Govindarajan V <govindarajanv@gmail.com>
2023-09-10 04:50:01 +0000

    state design pattern

---
+++ python/behavioural-design-patterns/state.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+class NotificationMode(ABC):
+    
+    @abstractmethod
+    def check(self, ctx):
+        pass
+
+class Vibration(NotificationMode):
+    def check(self, ctx):
+        print("Phone will vibrate")
+        
+class Silent(NotificationMode):
+    def check(self, ctx):
+        print("Notifications will be silenced")
+
+class Ring(NotificationMode):
+    def check(self, ctx):
+        print("Phone will ring")
+        
+# context
+class AlertSetting:
+    def __init__(self):
+        self.currentState = Ring()
+
+    def setState(self, state):
+        self.currentState = state
+
+    def check(self):
+        self.currentState.check(self)
+
+if __name__ == "__main__":
+    setting = AlertSetting()
+    setting.check()
+    setting.setState(Vibration())
+    setting.check()
+    setting.setState(Silent())
+    setting.check()
