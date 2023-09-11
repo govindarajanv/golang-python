c024ef9c0473cc0eb38644d006c701786b433feb
Govindarajan V <govindarajanv@gmail.com>
2023-09-11 04:49:28 +0000

    observer and template method

--- python/behavioural-design-patterns/observer.py
+++ python/behavioural-design-patterns/observer.py
@@ -1,1 +1,48 @@
 # define a subscription mechanism to notify multiple objects about any events that happen to the object they’re observing
+class Subscriber:
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def notify(self,topic, *args):
+        print (f"{self.name} got", args,"from",topic.name)
+        
+class SNSTopic:
+    
+    def __init__(self,name):
+        self.__name = name
+        self.__subscribers = []
+    
+    @property    
+    def name(self):
+        return self.__name
+        
+    def subscribe(self,subscriber):
+        self.__subscribers.append(subscriber)
+        
+    def unsubscribe(self,subscriber):
+        self.__subscribers.remove(subscriber)
+        
+    def notify_subscribers(self, *args):
+        for subs in self.__subscribers:
+            subs.notify(self,args)
+if __name__ == "__main__":
+    topic1 = SNSTopic("topicA")
+    topic2 = SNSTopic("topicB")
+    
+    sqs1 = Subscriber("sqsA")
+    lambda1 = Subscriber("lambdaA")
+    
+    topic1.subscribe(sqs1)
+    topic1.subscribe(lambda1)
+    
+    topic2.subscribe(lambda1)
+    
+    topic1.notify_subscribers("Broadcast 1:Processing is completed")
+    topic2.notify_subscribers("Broadcast 2:Processing is completed")
+    
+    topic1.unsubscribe(lambda1)
+    print (topic1.name, "unsubscribed by", lambda1.name)
+
+    topic1. notify_subscribers("Broadcast 3: Processing failed")
+    topic2. notify_subscribers("Broadcast 4: Processing failed")
--- python/behavioural-design-patterns/template-method.py
+++ python/behavioural-design-patterns/template-method.py
@@ -1,1 +1,64 @@
 # defines the skeleton of an algorithm in the superclass but lets subclasses override specific steps of the algorithm without changing its structure
+from abc import ABC, abstractmethod
+
+class Observability(ABC):
+    
+    @abstractmethod
+    def capture(self):
+        pass
+    
+    @abstractmethod
+    def publish(self):
+        pass
+    
+    @abstractmethod
+    def view(self):
+        pass
+    
+    # template method 
+    def manage(self):
+        self.capture()
+        self.publish()
+        self.view()
+    
+class Metric(Observability):
+    
+    def capture(self):
+        print (f"{self.__class__.__name__} is captured")
+        
+    def publish(self):
+        print (f"{self.__class__.__name__} is published")
+        
+    def view(self):
+        print (f"{self.__class__.__name__} is viewed")
+        
+class Log(Observability):
+    
+    def capture(self):
+        print (f"{self.__class__.__name__} is captured")
+        
+    def publish(self):
+        print (f"{self.__class__.__name__} is published")
+        
+    def view(self):
+        print (f"{self.__class__.__name__} is viewed")
+        
+class Trace(Observability):
+    
+    def capture(self):
+        print (f"{self.__class__.__name__} is captured")
+        
+    def publish(self):
+        print (f"{self.__class__.__name__} is published")
+        
+    def view(self):
+        print (f"{self.__class__.__name__} is viewed")
+        
+if __name__ == "__main__":
+    cpu_usage = Metric()
+    app_log = Log()
+    api_trace = Trace()
+    
+    cpu_usage.manage()
+    app_log.manage()
+    api_trace.manage()
