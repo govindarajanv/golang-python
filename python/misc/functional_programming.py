2f2933b8aabaf751b77e464d41b2dd6269ba6a70
Govindarajan V <govindarajanv@gmail.com>
2023-09-16 06:03:50 +0000

    functional programming

--- python/misc/functional_programming.py
+++ python/misc/functional_programming.py
@@ -1,6 +1,6 @@
 numbers = [9, 3, 49, 6, 28, 5, 20, 0, 3, 7,4, 2, 23, 34, 1, 8, 35]
 
-# create a list with odd numbers > 7 using list comprehension
+# Create a list with odd numbers > 7 using list comprehension
 odd_gt_7 = [num if num > 7 else 0 for num in numbers if num % 2 != 0 ]
 print ("odd_gt_7",odd_gt_7)
 
@@ -8,7 +8,7 @@ print ("odd_gt_7",odd_gt_7)
 squares = list (map(lambda num: num ** 2,numbers))
 print ("squares",squares)
 
-# create a list with even numbers less than 7 from the original list with filter function
+# Create a list with even numbers less than 7 from the original list with filter function
 even_lt_7 = list(filter(lambda num: num % 2 == 0 and num < 7,numbers))
 print ("even_lt_7",even_lt_7)
 
