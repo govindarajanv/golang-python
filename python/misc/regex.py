26abcbcb0bd6d9e4b09a837ede9224491f79ce87
Govindarajan V <govindarajanv@gmail.com>
2023-09-22 08:20:35 +0000

    regex

--- python/misc/regex.py
+++ python/misc/regex.py
@@ -1,29 +1,49 @@
 import re
-def validate_name(string):
-    only_alpha = re.compile(r'[^a-zA-Z]')
-    string = only_alpha.search(string)
-    return not bool(string)
 
-def validate_name(string):
-    only_alpha = re.compile(r'[^a-zA-Z]')
-    string = only_alpha.search(string)
-    return not bool(string)
-
-def look_for_failure(string):
-    value = re.match ("(\w+) (\w+)", string)
-    print (value.groups())
+class PatternFinder:
     
-    print (re.findall(r"(\w+) (\w+)", string))
-    #r1 = re.findall(r"^\w+",string)
-    failure = re.search(r'fail')
-    return not bool(failure)
+    def __init__(self, text):
+        self.__text = text
     
-print(validate_name("ABCDEFabcdef")) 
-print(validate_name("*&%@#!}{"))
-
-test_str = "fail there is some error."
-value = re.match ("(\w+) (\w+)", test_str)
-print ("groups:",value.groups())
-print ("findall:",re.findall(r"(\w+) (\w+)", test_str))
-result = re.search(r'fail',test_str)
-print ("search:",result.group(0))
+    def find_begin(self,pattern):
+        match_object = re.match(pattern, self.__text)
+        if match_object:
+            print("Match found!")
+        else:
+            print("No match found!")
+ 
+    def find_any(self,pattern):
+        match_object = re.search(pattern, self.__text)
+        if match_object:
+            print("Match found!")
+        else:
+            print("No match found!") 
+            
+    def replace(self,find,replace):
+        result = re.sub(find,replace, self.__text)
+        print (result)
+ 
+            
+    def find_all(self,pattern):
+        matches = re.findall(pattern, self.__text)
+        print (matches)
+        
+    def find_list(self,pattern):
+        match_iterator = re.finditer(pattern, self.__text)
+        for match_object in match_iterator:
+            print(match_object.group(),"start=",match_object.start(),"end=",match_object.end())
+            
+    def compile(self,pattern):
+        keyword = re.compile(pattern)
+        exists = keyword.search(self.__text)
+        if exists:
+            print (f"pattern \"{pattern}\" exists",exists.group(0))
+            
+if __name__ == "__main__":
+    pf = PatternFinder("I love Python. Python is easy to learn, and there are fun ways to learn Python ")
+    pf.find_begin('Python')
+    pf.find_any('Python')
+    pf.find_all('Python')
+    pf.find_list('Python')
+    pf.replace(r'Python',r'Golang')
+    pf.compile('Python')
