33611810c604542d24d0fcc6600970630b815d97
Govindarajan V <govindarajanv@gmail.com>
2023-10-04 09:57:03 +0000

    cpu bound and io bound programs

--- python/concurrency_parallelism/io_bound.py
+++ python/concurrency_parallelism/io_bound.py
@@ -6,34 +6,72 @@ import threading
 import asyncio
 import aiohttp
 
+# multithreading
 thread_local = threading.local()
 
+# Sync version
 def download_site_sync(url, session):
     with session.get(url) as response:
-        print(f"Read {len(response.content)} from {url}")
-
+        #print(f"Read {len(response.content)} from {url}")
+        pass
 
 def download_all_sites_sync(sites):
     with requests.Session() as session:
         for url in sites:
             download_site_sync(url, session)
 
+# multithreading
+def get_session_mt():
+    if not hasattr(thread_local, "session"):
+        thread_local.session = requests.Session()
+    return thread_local.session
+
+def download_site_mt(url):
+    
+    session = get_session_mt()
+    with session.get(url) as response:
+        #print(f"Read {len(response.content)} from {url}")
+        pass
+
+def download_all_sites_mt(sites):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download_site_mt, sites)
+
+# multiprocessing
+def download_site_mp(url):
+    session = requests.Session()
+    with session.get(url) as response:
+        #print(f"Read {len(response.content)} from {url}")
+        pass
+
+
+def download_all_sites_mp(sites):
+    with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
+        executor.map(download_site_mp, sites)
+
+# async
 async def download_site_async(session, url):
     async with session.get(url) as response:
         print("Read {0} from {1}".format(response.content_length, url))
+        #pass
 
 
 async def download_all_sites_async(sites):
     async with aiohttp.ClientSession() as session:
         tasks = []
         for url in sites:
-            task = asyncio.ensure_future(download_site(session, url))
+            task = asyncio.ensure_future(download_site_async(session, url))
             tasks.append(task)
         await asyncio.gather(*tasks, return_exceptions=True)
 
-def download(func,sites):
+def download(func,sites,asyncio_flag = False):
     start_time = time.time()
-    func(sites)
+    if not asyncio_flag:
+        func(sites)
+    else:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(func(sites))
     duration = time.time() - start_time
     print(f"{func.__name__}: Downloaded {len(sites)} in {duration} seconds")
 
@@ -43,3 +81,6 @@ if __name__ == "__main__":
         "http://olympus.realpython.org/dice",
     ] * 80
     download(download_all_sites_sync,sites)
+    download(download_all_sites_mt,sites)
+    download(download_all_sites_mp,sites)
+    download(download_all_sites_async,sites,True)
