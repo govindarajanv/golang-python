c866ba2cf9d9a1d161905ef1c4135352819af9fc
Govindarajan V <govindarajanv@gmail.com>
2023-10-27 12:02:57 +0000

    prototype design pattern

--- python/creational-design-patterns/prototype.py
+++ python/creational-design-patterns/prototype.py
@@ -4,46 +4,40 @@ class Car:
     """ Car prototype """
 
     def __init__(self):
-        self.__engine = "3000cc"
-        self.__model = "prototype"
-        self.__color = "Red"
-        self.__seats = 5
-        self.__type = "Hatchback"
-
+        self.__engine_no = None
+        self.engine = "1100cc"
+        self.make = "Maruti"
+        self.model = "prototype" # this is important
+        self.variant = None
+        
     def __str__(self):
-        return f"Car model: {self.__model} with engine: {self.__engine}, color: {self.__color}, seats: {self.__seats}, type: {self.__type}"
-
-class PrototypeFactory:
-
-
+        return f"{self.__class__.__name__}/ {self.__engine_no}/ {self.engine}/  {self.make} / {self.model} / {self.variant}"
+    
+class PrototypeBodyBuildingFactory:
+    
     def __init__(self):
         self.__instances = {}
-
-    def registerObject(self,name,obj):
+        
+    def registerObject(self,name, obj):
         self.__instances[name] = obj
 
-    def unregisterObject(self,name):
+    def deregisterObject(self,name):
         del self.__instances[name]
-
-
+        
     def clone(self,name,**kwargs):
         clonedObject = copy.deepcopy(self.__instances[name])
         # this is also very important in addition to deepcopy
-        clonedObject.__dict__.update(kwargs)
+        clonedObject.__dict__.update(**kwargs)
         return clonedObject
-
+        
 if __name__ == "__main__":
-    car = Car()
     
-    prototypeFactory = PrototypeFactory()
-
-    prototypeFactory.registerObject('basicCar',car)
-
-    i10 = prototypeFactory.clone("basicCar",_Car__model="i10")
-    print (i10)
-    creta = prototypeFactory.clone("basicCar",_Car__color="Blue",_Car__type="SUV",_Car__model="Creta")
-    print (creta)
-
-
-
-
+    chassis = Car()
+    
+    pf = PrototypeBodyBuildingFactory()
+    pf.registerObject("hatchback",chassis)
+    car1 = pf.clone("hatchback",variant="Petrol",_Car__engine_no="12345",model="Swift")
+    #print (car1)
+    car2 = pf.clone("hatchback",variant="Diesel",_Car__engine_no="12346",model="Ritz")
+    print (car1, car2,sep="\n")
+    pf.deregisterObject("hatchback")
