0e18f92eb70afa4952911537f96b47a35c6bb9d3
Govindarajan V <govindarajanv@gmail.com>
2023-10-27 11:07:16 +0000

    design patterns

--- python/creational-design-patterns/builder.py
+++ python/creational-design-patterns/builder.py
@@ -1,53 +1,56 @@
 class VM:
-
     def __init__(self,name):
         self.name = name
-        self.image = None
         self.cpu = None
-        self.disk = None
         self.memory = None
-
-    def __str__(self):
-        return f"{self.name}, {self.image}, {self.cpu} CPUs, {self.disk} Disk, {self.memory} RAM"
-
-class VMBuilder:
-    def __init__(self,name):
-        self.VM = VM(name)
+        self.disk = None
+        self.image = None
     
+    def __str__(self):
+        return f"{self.name} VM created with CPU: {self.cpu}, Memory: {self.memory}, Disk: {self.disk}, Image: {self.image}"
+        
+class VMBuilder:
+    
+    def __init__(self,name):
+        self.vm = VM(name)
+        
     def configure_image(self,image):
-        self.VM.image = image
-
+        self.vm.image = image
+        
     def configure_cpu(self,cpu):
-        self.VM.cpu = cpu
-
+        self.vm.cpu = cpu
+        
     def configure_memory(self,memory):
-        self.VM.memory = memory
-
+        self.vm.memory = memory
+        
     def configure_disk(self,disk):
-        self.VM.disk = disk
+        self.vm.disk = disk
 
-# Director
+# Director        
 class CLI:
+    
     def __init__(self):
         self.builder = None
-
-    def build_vm(self,name):
+        
+    def build_vm(self,name,image,disk,memory,cpu):
         self.builder = VMBuilder(name)
-        self.builder.configure_cpu("2")
-        self.builder.configure_disk("100GB")
-        self.builder.configure_image("ami123456")
-        self.builder.configure_memory("16GB")
+        
+        self.builder.configure_image(image)
+        self.builder.configure_disk(disk)
+        self.builder.configure_memory(memory)
+        self.builder.configure_cpu(cpu)
     
-    # The user can use now use the function the attribute 
     @property
     def VM(self):
-        return self.builder.VM
-
+        return self.builder.vm
+        
 if __name__ == "__main__":
-    cli = CLI()
-    cli.build_vm("ec2")
-
-    print (cli.VM)
+    
+    awscli = CLI()
+    awscli.build_vm("ec2","Amazon Linux","500 GB","16 GB","2 cores")
+    print (awscli.VM)
+    awscli.build_vm("ec2","CentOS","200 GB","32 GB","4 cores")
+    print (awscli.VM)  
 
 
 
