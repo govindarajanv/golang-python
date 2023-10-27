228cda378cad8af0215dad6a374a19bb67011baf
Govindarajan V <govindarajanv@gmail.com>
2023-10-27 12:45:37 +0000

    abstract factory design pattern

--- python/creational-design-patterns/abstract_factory.py
+++ python/creational-design-patterns/abstract_factory.py
@@ -1,7 +1,7 @@
 from abc import ABC,abstractmethod
 from enum import Enum,auto
 
-class HotDrinks(ABC):
+class HotDrinks:
     
     @abstractmethod
     def consume(self):
@@ -20,47 +20,54 @@ class Tea(HotDrinks):
 class HotDrinksFactory(ABC):
     
     @abstractmethod
-    def prepare(self,quantity):
+    def prepare(self):
         pass
     
 class CoffeeFactory(HotDrinksFactory):
     
-    def prepare(self,quantity):
+    def prepare(self):
         return Coffee()
         
 class TeaFactory(HotDrinksFactory):
     
-    def prepare(self,quantity):
-        return Tea()
-        
+    def prepare(self):
+        return Tea()        
+
 class HotDrinksMachine:
-    
     class AvailableDrinks(Enum):
         COFFEE = auto()
         TEA = auto()
-        
+    
     __initialized = False
     __factories = {}
     
+    # get a dictionary of <beverage_name> : <its factory>
     def __init__(self):
         if not self.__initialized:
             self.__initialized = True
             for drink in self.AvailableDrinks:
-                factory = drink.name[0] + drink.name[1:].lower() + "Factory"
+                factory = drink.name[0].upper()+drink.name[1:].lower() + "Factory"
                 self.__factories[drink.name] = factory
-                
-    def prepare(self):
-        
+            print (self.__factories)
+    
+    # get the choice, parse the available drinks to arrive at factories, call factory to get beverage 
+    def dispense(self):
         choice = input("Please choose your beverage. [Coffee/Tea]:")
-        #assert choice.lower() == "coffee" or choice.lower() == "tea", "Invalid option"
-        available_drinks = [member.name for member in self.AvailableDrinks]
+        
+        # assert choice.lower() == "coffee" or choice.lower() == "tea", "Invalid Option
+        
+        available_drinks = self.AvailableDrinks._member_names_
+        # available_drinks = [ member.name for member in self.AvailableDrinks ] # this is also an option
         if choice.upper() in available_drinks:
-            factory = eval(self.__factories[choice.upper()])()
-            return factory.prepare(200)
+            factory_name = self.__factories[choice.upper()]
+            factory_obj = eval(factory_name)()
+            beverage = factory_obj.prepare()
+            return beverage
         else:
             raise AssertionError("Invalid Option")
-            
+    
+    
 if __name__ == "__main__":
-    hd = HotDrinksMachine()
-    beverage = hd.prepare()
+    hm = HotDrinksMachine()
+    beverage = hm.dispense()
     beverage.consume()
