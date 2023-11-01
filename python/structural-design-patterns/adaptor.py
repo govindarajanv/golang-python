70aeedd53c86695e2d4192affcf15fe8479f8e30
Govindarajan V <govindarajanv@gmail.com>
2023-11-01 07:27:16 +0000

    refactored adaptor pattern

--- python/structural-design-patterns/adaptor.py
+++ python/structural-design-patterns/adaptor.py
@@ -3,47 +3,44 @@ class MobileAdapter:
         self.out_voltage = 0
         self.out_ampere = 0
 
-    def transform(self,in_voltage,in_ampere):
-        if in_voltage == 220:
+    def transform(self, in_voltage, in_ampere):
+        if in_voltage == 220 and in_ampere == 5:
             self.out_voltage = 5
-        else:
-            self.out_voltage = 0
-        if in_ampere == 5:
             self.out_ampere = 2.1
         else:
+            self.out_voltage = 0 
             self.out_ampere = 0
-
-
-class  Socket:
+        
+class Socket:
     def __init__(self):
         self.adapter = None
-
+        
     def connect(self,adapter):
         self.adapter = adapter
-
+    
     def power_on(self):
-        if self.adapter:
-            self.adapter.transform(220,5)
+        self.adapter.transform(220,5)
+    
     def power_off(self):
-        if self.adapter:
-            self.adapter.transform(0,0)
+        self.adapter.transform(0,0)
 
 class Mobile:
     def __init__(self):
         self.adapter = None
-        self.__voltage = 5
-        self.__ampere = 2.1
-        self.__charging = False
+        self.in_ampere = 2.1 
+        self.in_voltage = 5
 
+        
     def connect(self,adapter):
         self.adapter = adapter
-
+        
     def charge_status(self):
-        if self.adapter.out_voltage == self.__voltage and self.adapter.out_ampere == self.__ampere:
-            self.__charging = True
+        if self.in_voltage == self.adapter.out_voltage and self.in_ampere == self.adapter.out_ampere:
+            charging = True
         else:
-            self.__charging = False
-        return f"\n-> Mobile charging: {self.__charging}\n"
+            charging = False
+            
+        return f"\n-> Mobile charging: {charging}\n"
 
 if __name__ == "__main__":
 
@@ -54,10 +51,10 @@ if __name__ == "__main__":
     adapter = MobileAdapter()
 
     mobile.connect(adapter)
-    print ("mobile connected to adapter")
+    print ("\nmobile connected to adapter")
 
     socket.connect(adapter)
-    print ("socket connected to adapter")
+    print ("\nsocket connected to adapter")
 
     print (mobile.charge_status())
 
