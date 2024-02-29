cfec3c2c407953fcfabb1c50e019ec2397187e2e
Govindarajan V <govindarajanv@gmail.com>
2024-02-29 15:07:41 +0530

    Update logger.py

--- python/misc/logger.py
+++ python/misc/logger.py
@@ -1,7 +1,7 @@
 import logging
 
 # Create and configure logger
-logging.basicConfig(filename="newfile.log",format='%(asctime)s %(message)s',filemode='w')
+logging.basicConfig(filename="newfile.log",format="%(levelname)s %(asctime)s %(message)s",filemode="w") 
 
 # Creating an object
 logger = logging.getLogger()
