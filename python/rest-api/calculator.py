793ed0f2e3c4644910f316f3ce7cd1b566c5e443
Govindarajan V <govindarajanv@gmail.com>
2024-03-03 17:15:09 +0530

    Update calculator.py

--- python/rest-api/calculator.py
+++ python/rest-api/calculator.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, jsonify, request
 
 # creating a Flask app
@@ -12,74 +11,53 @@ def validate_data(args):
     else:
         return "Success", 200
 
+def calculate(args,operand):
+    msg, status_code = validate_data(request.args)
+    result = 0.0
+    if status_code == 200:
+        num1  = float(request.args.get('num1'))
+        num2  = float(request.args.get('num2'))
+
+        if operand == "add":
+            result = num1 + num2
+        elif operand == "sub":
+            result = num1 - num2
+        elif operand == "mul":
+            result = num1 * num2
+        elif operand == "div":
+            result = num1 / num2
+    ret = {
+        'status': status_code,
+        'result': result,
+        'message': msg
+        }
+    return jsonify(ret)
+
+
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-	if(request.method == 'GET'):
+        if(request.method == 'GET'):
 
-		data = "This a calculator app"
-		return jsonify({'data': data})
+                data = "This a calculator app"
+                return jsonify({'data': data})
 
 @app.route('/add', methods = ['GET'])
 def add():
-    msg, status_code = validate_data(request.args)
-    result = 0.0
-    if status_code == 200:
-        num1  = request.args.get('num1')
-        num2  = request.args.get('num2')
-        result = float(num1) + float(num2)
-    ret = {
-        'status': status_code,
-        'result': result,
-        'message': msg
-    }
-    return jsonify(ret)
+    return calculate(request.args,"add")
 
 @app.route('/sub', methods = ['GET'])
 def sub():
-    msg, status_code = validate_data(request.args)
-    result = 0.0
-    if status_code == 200:
-        num1  = request.args.get('num1')
-        num2  = request.args.get('num2')
-        result = float(num1) - float(num2)
-    ret = {
-        'status': status_code,
-        'result': result,
-        'message': msg
-    }
-    return jsonify(ret)
+    return calculate(request.args,"sub")
 
 @app.route('/mul', methods = ['GET'])
 def mul():
-    msg, status_code = validate_data(request.args)
-    result = 0.0
-    if status_code == 200:
-        num1  = request.args.get('num1')
-        num2  = request.args.get('num2')
-        result = float(num1) * float(num2)
-    ret = {
-        'status': status_code,
-        'result': result,
-        'message': msg
-    }
-    return jsonify(ret)
+    return calculate(request.args,"mul")
 
 @app.route('/div', methods = ['GET'])
 def div():
-    msg, status_code = validate_data(request.args)
-    result = 0.0
-    if status_code == 200:
-        num1  = request.args.get('num1')
-        num2  = request.args.get('num2')
-        result = float(num1) / float(num2)
-    ret = {
-        'status': status_code,
-        'result': result,
-        'message': msg
-    }
-    return jsonify(ret)
+    return calculate(request.args,"div")
 
 
 if __name__ == '__main__':
 
-	app.run(debug = True,port=5000)
+        app.run(debug = True,host="0.0.0.0",port=3000)
